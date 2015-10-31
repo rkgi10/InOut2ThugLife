b71084cf8c0679e48e4009b6b19f2d5bfe12667b
Rohit Gurnani <rkgi10@gmail.com>
2015-10-31 16:42:53 +0530

    Initial Commit

---
+++ Inout2ThugLife/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Inout2ThugLife
+//
+//  Created by Rohit Gurnani on 31/10/15.
+//  Copyright © 2015 Rohit Gurnani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Inout2ThugLifeTests/Inout2ThugLifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Inout2ThugLifeTests.swift
+//  Inout2ThugLifeTests
+//
+//  Created by Rohit Gurnani on 31/10/15.
+//  Copyright © 2015 Rohit Gurnani. All rights reserved.
+//
+
+import XCTest
+@testable import Inout2ThugLife
+
+class Inout2ThugLifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
