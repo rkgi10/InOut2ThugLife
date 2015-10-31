da2cfbeefaebb442fcde382b9d16f184840bcaaa
Rohit Gurnani <rkgi10@gmail.com>
2015-10-31 20:50:31 +0530

    Initial commit with basic product setup.

---
+++ Inout2ThugLife/Medicine.swift
@@ -0,0 +1,29 @@
+//
+//  Medicine.swift
+//  Inout2ThugLife
+//
+//  Created by Rohit Gurnani on 31/10/15.
+//  Copyright © 2015 Rohit Gurnani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Medicine {
+    var name : String?
+    var category : String?
+    var price : Double?
+    var cures : [String]
+    var isAvailableWithoutPrescription : Bool?
+    var image : UIImage?
+    
+    init(name : String?, category : String?, price : Double?, cures : [String], isAvailableWithoutPrescription : Bool? , image : UIImage?)
+    {
+        self.name = name
+        self.category = category
+        self.price = price
+        self.cures = cures
+        self.isAvailableWithoutPrescription = isAvailableWithoutPrescription
+        self.image = image
+    }
+    
+}
---
+++ Inout2ThugLife/MedicinesampleData.swift
@@ -0,0 +1,9 @@
+//
+//  MedicinesampleData.swift
+//  Inout2ThugLife
+//
+//  Created by Rohit Gurnani on 31/10/15.
+//  Copyright © 2015 Rohit Gurnani. All rights reserved.
+//
+
+import Foundation
